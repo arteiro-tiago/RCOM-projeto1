5a43113df12a3f5e38553c30352c9e2104635faf
arteiro-tiago <tiagoarteiro1@gmail.com>
2025-10-16 12:25:31 +0100

    Frame Start a ser enviado com erro na leitura

--- src/application_layer.c
+++ src/application_layer.c
@@ -4,10 +4,35 @@
 
 #include <stdio.h>
 
+#define C_START 1
+#define C_DATA 2
+#define C_END 3
+
+#define T_SIZE 0
+#define BUF_SIZE 256
+
+
+void getStartControlPacket(FILE* file, unsigned char * buf){
+    fseek(file,0,SEEK_END);
+    int filesize = ftell(file);
+    fseek(file,0,SEEK_SET);
+    buf[0] = C_START; 
+    buf[1] = T_SIZE;
+    int filesizeback = filesize;
+    int L1 = 0;
+    while(filesizeback>0){
+        L1++;
+        filesizeback-=256;
+    }
+    buf[2] = L1;
+}   
+
 void applicationLayer(const char *serialPort, const char *role, int baudRate,
                       int nTries, int timeout, const char *filename)
 {
     LinkLayer connection;
+    unsigned char buf[BUF_SIZE];
+
     strcpy(connection.serialPort, serialPort);    
     if (strcasecmp(role, "rx") == 0) {
         connection.role = LlRx;
@@ -15,10 +40,26 @@ void applicationLayer(const char *serialPort, const char *role, int baudRate,
     else {
         connection.role = LlTx;
     }
-
+    
     connection.baudRate = baudRate;
     connection.nRetransmissions = nTries;
     connection.timeout = timeout;
     
-    llopen(connection);
+    if (llopen(connection) == 1){
+        return;
+    }
+    switch (connection.role){
+        case (LlTx):
+            FILE* file = fopen(filename,"rb");
+            getStartControlPacket(file, buf);
+
+            llwrite(buf, BUF_SIZE-6, connection);
+
+            break;
+        case (LlRx):
+            //TO_DO
+            break;
+    }    
 }
+
+
--- src/link_layer.c
+++ src/link_layer.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h> // added for memcpy
 
 // MISC
 #define _POSIX_SOURCE 1 // POSIX compliant source
@@ -21,7 +22,11 @@
 #define STATE_A_RCV 2
 #define STATE_C_RCV 3
 #define STATE_BCC_OK 4
-#define STATE_STOP 5
+#define STATE_DATA_ALL 5
+#define STATE_BCC2_OK 6
+#define STATE_STOP 7
+
+int C_RR = 0;
 
 volatile int STOP = FALSE;
 int alarmCount = 0;
@@ -33,6 +38,28 @@ void alarmHandler(int signal)
     alarmCount++;
 }
 
+void printBufferNamed(const unsigned char *buffer, int size) {
+    printf("---- Buffer Dump (%d bytes) ----\n", size);
+    for (int i = 0; i < size; i++) {
+        unsigned char byte = buffer[i];
+        const char *name = NULL;
+
+        // Match special byte names
+        switch (byte) {
+            case FLAG: name = "FLAG"; break;
+            case A_TX: name = "A_TX"; break;
+            case A_RX: name = "A_RX"; break;
+            case C_UA: name = "C_UA"; break;
+            default: name = NULL; break;
+        }
+
+        if (name)
+            printf("[%02d]: 0x%02X  (%3d)  <-- %s\n", i, byte, byte, name);
+        else
+            printf("[%02d]: 0x%02X  (%3d)\n", i, byte, byte);
+    }
+    printf("-------------------------------\n");
+}
 
 ////////////////////////////////////////////////
 // LLOPEN
@@ -96,6 +123,7 @@ int receiveSET(LinkLayer connectionParameters){
     }
     return 1;
 }
+
 int sendSET(LinkLayer connectionParameters){
     unsigned char buf[BUF_SIZE] = {0};
     buf[0] = FLAG;
@@ -218,6 +246,7 @@ int llopen(LinkLayer connectionParameters)
             }
             if(alarmCount == connectionParameters.nRetransmissions){
                 printf("erro de ligação\n");
+                return 1;
             }
             break;
         
@@ -225,15 +254,142 @@ int llopen(LinkLayer connectionParameters)
     return 0;
 }
 
+ 
+int sendIFrame(const unsigned char *buf, int bufSize){
+    int iframeLen = bufSize + 6; // FLAG,A,C,BCC1,data,BCC2,FLAG
+    unsigned char iframe[BUF_SIZE + 6] = {0}; // safe fixed max
+    iframe[0] = FLAG;
+    iframe[1] = A_TX;
+    iframe[2] = C_RR;
+    iframe[3] = iframe[1] ^ iframe[2]; // BCC1
+    memcpy(iframe + 4, buf, bufSize);
+    unsigned char BCC2 = 0;
+    for (int i = 0; i < bufSize; i++) {
+        BCC2 ^= buf[i];
+    }
+    iframe[4 + bufSize] = BCC2;
+    iframe[5 + bufSize] = FLAG;
+
+    printBufferNamed(iframe, iframeLen);
+    writeBytesSerialPort(iframe, iframeLen);
+    printf("IFrame Sent\n");
+    sleep(1);
+    return 0;
+}
+
+int receiveIFRame(int bufSize){
+    int d = 0;
+    int nBytesBuf = 0;
+    int state = STATE_START;
+    unsigned char byte;
+    unsigned char BCC2;
+
+    while (state != STATE_STOP && alarmEnabled){
+        int res = readByteSerialPort(&byte);
+        printf("%d\n", state);
+        if (res == 0)
+            continue;
+        switch (state)
+        {
+        case STATE_START:
+            if (byte == FLAG)
+                state = STATE_FLAG_RCV;
+            break;
+
+        case STATE_FLAG_RCV:
+            if (byte == A_RX)
+                state = STATE_A_RCV;
+            else if (byte == FLAG)
+                state = STATE_FLAG_RCV;
+            else 
+                state = STATE_START;
+            break;
+
+        case STATE_A_RCV:
+            if (byte == C_UA)
+                state = STATE_C_RCV;
+            else if (byte == FLAG)
+                state = STATE_FLAG_RCV;
+            else
+                state = STATE_START;
+            break;
+
+        case STATE_C_RCV:
+            if (byte == (A_RX ^ C_UA))
+                state = STATE_BCC_OK;
+            else if (byte == FLAG)
+                state = STATE_FLAG_RCV;
+            else
+                state = STATE_START;
+            break;
+
+        case STATE_BCC_OK:
+            if (d == 0) {
+                BCC2 = byte; 
+                d++;
+            }
+            else {
+                BCC2 ^= byte;
+                d++;
+            }
+            if(d == bufSize){
+                state = STATE_DATA_ALL; 
+            }
+            break;
+        case STATE_DATA_ALL:
+            if (byte == BCC2){
+                state = STATE_BCC2_OK;
+            }
+            else state = STATE_START;
+            break;
+        case STATE_BCC2_OK:
+            if (byte == FLAG)
+            {
+                printf("Ligação estabelecida\n");
+                state = STATE_STOP;
+                nBytesBuf += res;
+                return 0;
+            }
+            else
+                state = STATE_START;
+            break;
+        }
 
 
+    }
+    return 1;
+}
 ////////////////////////////////////////////////
 // LLWRITE
 ////////////////////////////////////////////////
-int llwrite(const unsigned char *buf, int bufSize)
+int llwrite(const unsigned char *buf, int bufSize, LinkLayer connectionParameters)
 {
-    // TODO: Implement this function
+    struct sigaction act = {0};
+    act.sa_handler = &alarmHandler;
+    if (sigaction(SIGALRM, &act, NULL) == -1)
+    {
+        perror("sigaction");
+        exit(1);
+    }
 
+    printf("Alarm configured\n");
+    while(alarmCount < connectionParameters.nRetransmissions){
+        if (alarmEnabled == FALSE)
+        {
+            alarm(connectionParameters.timeout);
+            alarmEnabled = TRUE;
+        }
+        sendIFrame(buf, bufSize);
+        if(receiveIFRame(bufSize) == 0){
+            alarmEnabled = FALSE;
+            alarmCount = 0;
+            break;
+        }
+    }
+    if(alarmCount == connectionParameters.nRetransmissions){
+        printf("erro de ligação\n");
+        return 1;
+    }
     return 0;
 }
 
